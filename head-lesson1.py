c39395da8a882ff4f3fcb5bf70ac9d7f1b4b5e58
feiyun1997 <xingxi21@sina.com>
2013-08-25 09:12:02 +0800

    first lesson
    2013-8-25
    head first python's note,step by step.

---
+++ head-lesson1.py
@@ -0,0 +1,14 @@
+﻿man=[]
+said=[]
+try:
+	data=open('zy.txt','w')
+	for line in data:
+		try:
+			(r,said1)=data.split(':')
+			print 'Man say:',
+			print '\t'
+			print 'fine',said1
+		except ValueError :
+			print 'value is wrong'
+except IOError:
+	print 'no file'
